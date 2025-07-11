633b3af1b220f5334155c11723d4d0baeb19aeb0
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2025-07-12 00:29:26 +0600

    toastyfy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react"
 import Banner from "./components/Banner/Banner"
 import Navbar from "./components/Navbar/Navbar"
+import Swal from 'sweetalert2'
 
 
 function App() {
-  const [credits,setCredits]=useState(0);
-  const claimCredit=()=>{
+  const [credits, setCredits] = useState(0);
+  const claimCredit = () => {
     console.log("Button Pressed")
-    setCredits(credits+600000);
+    setCredits(credits + 600000);
+    Swal.fire({
+
+      text: 'You are adding coin',
+      confirmButtonText: 'ok'
+    })
   }
 
   return (
