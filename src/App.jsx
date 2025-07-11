63532635dbbf55bd2d9f9ec6974160dd3b7594aa
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2025-07-11 21:47:51 +0600

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner/Banner"
 import Navbar from "./components/Navbar/Navbar"
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="max-w-7xl mx-auto mt-3">
       <Navbar></Navbar>
+      <Banner></Banner>
     </div>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cricket from'../../assets/images/banner-main.png'
+const Banner = () => {
+    return (
+      
+        <div className='w-full h-[300px] bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%'>
+            <div className='mx-auto'>
+                <img className='w-[248px] h-[200px] mx-auto' src={cricket} alt="" />
+                <h1 className='text-center text-2xl font-bold text-gray-400'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                <button className='block mx-auto bg-yellow-500 text-black font-bold px-4 py-2 rounded'>Claim free Credit</button>
+            </div>
+        </div>
+
+    );
+};
+
+export default Banner;
